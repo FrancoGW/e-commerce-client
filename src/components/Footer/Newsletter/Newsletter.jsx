6d25cc4f68721f5622805faf7705cb0493fdc2af
Franco Lautaro Garay <84424082+FrancoGW@users.noreply.github.com>
2023-04-14 22:03:34 -0300

    new filesss

--- src/components/Footer/Newsletter/Newsletter.jsx
+++ src/components/Footer/Newsletter/Newsletter.jsx
@@ -1,6 +1,38 @@
 import "./Newsletter.scss";
+import {
+    FaFacebookF,
+    FaTwitter,
+    FaInstagram,
+    FaLinkedinIn,
+}from 'react-icons/fa';
+import './Newsletter.scss'
 const Newsletter = () => {
-    return <div>Newsletter</div>;
+    return <div className="newsletter-section">
+        <div className="newsletter-content">
+            <span className="small-text">Newsletter</span>
+            <span className="big-text">Sign up for lastest updates and offers</span>
+            <div className="form">
+                <input type="text" placeholder="Email Address" name="" id="" />
+                <button>Suscribe</button>
+            </div>
+            <div className="text">will be used in accordace with our Privacy Policy</div>
+            <div className="social-icons">
+                <div className="icon">
+                    <FaFacebookF size={14}/>
+                </div>
+                <div className="icon">
+                    <FaTwitter size={14}/>
+                </div>
+                <div className="icon">
+                    <FaInstagram size={14}/>
+                </div> 
+                <div className="icon">
+                    <FaLinkedinIn size={14}/>
+                </div>
+            </div>
+        </div>
+
+    </div>;
 };
 
 export default Newsletter;
